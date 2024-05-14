f1b3e4314a982f3274234f3d1bef6fc0950fef87
Sperrotta10 <santiagoperrotta10@gmail.com>
2024-05-14 09:30:31 -0400

    manejo de errores, entorno virtual, modificaciones, mejorando codigo

--- Evaluacion_1/proyectos/interfaz.py
+++ Evaluacion_1/proyectos/interfaz.py
@@ -1,13 +1,14 @@
 import flet as ft
 import sistema_numerico as sn
+import matriz as gj
+import random
 
 def main(page: ft.Page):
 
     # informacion sobre la ventana, alto, ancho, posicion x, y
-    page.window_height = 500
+    page.window_height = 600
     page.window_width = 700
-    page.window_left = 330
-    page.window_top = 135
+    page.window_center()
 
 
     # Manejo de entrada de datos por teclado (Conversion de numeros)
@@ -42,7 +43,7 @@ def main(page: ft.Page):
             entrada.value = ""
             entrada.read_only = False
             entrada.input_filter.regex_string = r"[0-2]"
-            
+
         elif(lista1.value == "Cuaternario"):
 
             entrada.value = ""
@@ -127,37 +128,132 @@ def main(page: ft.Page):
 
     """==========================================================================================================================================================="""
     
+
+
     
     """======================================================= Ventana Secundaria (Gauss-Jordan) ================================================================="""
 
     # generamos los texfields dependiendo del tamano que asigne el usuario
     def generate_matrix(e):
 
-        tamano = int(txt_size.value)    # obtenemos el valor de las filas y columnas
-        matrix_container.controls = []  # Limpia los controles existentes
+        # validacion para que solamente se puede generar una matriz 2x2 en adelante
+        if(txt_size.value != ""):
+            if(int(txt_size.value) >= 2):
 
-        for i in range(tamano):
+                txt_size.read_only = True
+                tamano = int(txt_size.value)    # obtenemos el valor de las filas y columnas
+                matrix_container.controls = []  # Limpia los controles existentes
 
-            # usamos una lista por compresion para generar una serie de filas
-            # dentro de la lista por comprension tenemos dos condiciones, una para identificar el texfield para marcar la igualdad y que no se puede ingresar texto
-            row = ft.Row([ft.TextField(input_filter=ft.InputFilter(allow=True, regex_string = r"[0-9]", replacement_string=""),
-                        bgcolor= "#FFFFFF", width=60, height= 40, text_align = ft.TextAlign.CENTER, value = "=" if (j == tamano) else "",
-                        read_only = (j == tamano)) for j in range(tamano + 2)])
-            
-            matrix_container.controls.append(row)
+                for i in range(tamano):
+
+                    # usamos una lista por compresion para generar una serie de filas
+                    # dentro de la lista por comprension tenemos dos condiciones, una para identificar el texfield para marcar la igualdad y que no se puede ingresar texto
+                    row = ft.Row([ft.TextField(input_filter=ft.InputFilter(allow=True, regex_string = r"[0-9]", replacement_string=""),
+                                bgcolor= "#FFFFFF", width=60, height= 40, text_align = ft.TextAlign.CENTER, value = "=" if (j == tamano) else "",
+                                read_only = (j == tamano)) for j in range(tamano + 2)])
+                    
+                    matrix_container.controls.append(row)
+
+                page.update()
+            else:
+                pass
+        else:
+            pass
+
+    # metodo para borrar los campos de texto
+    def borrar_segunda_ventana(e):
+        txt_size.value = ""                  # vaciamos el campo del tamano de la matriz
+        txt_size.read_only = False           # Habilitamos el campo de texto
+        resultado_matriz.value = ""          # vaciamos el campo del resultado del sistema de ecuaciones
+        resultado_matriz.width = 260         # asignamos el ancho por defecto del campo de texto
+        matrix_container.controls = []       # vaciamos el contenedor donde se almacena todos los texfields de la matriz y el termino independiente
+        matriz_valores.clear()
 
         page.update()
 
+    def validacion_campos_vacios(matrix_container,txt):
 
+        bandera = False
+        cont_elementos = 0
+
+        # verificamos si los campos de textos de la matriz y el termino indenpendiente estan vacios
+        for fila in matrix_container.controls:
+            for texfield in fila.controls:
+                if(texfield.value != ""):
+                    cont_elementos += 1
+
+        # sabemos que si no se proporciono el tamano de la matriz, mucho menos va a existir la misma, por eso debemos comprobar si el campo esta vacio
+        # y si no lo esta ahi verificamos si cada uno de los elementos de la matriz y termino independiente estan rellenados si es asi soltamos un True
+        if(txt_size.value != ""):
+            if(cont_elementos == (int(txt_size.value) * int(txt_size.value)) + int(txt_size.value) * 2):
+                bandera = True
+
+        return bandera
+
+
+    # metodo con el cual vamos obtener los resultados
     def obtener_valores_matriz(e):
 
-        for fila in matrix_container.controls:
-            fila_valores = []
-            for texfield in fila.controls:
-                fila_valores.append(texfield.value)
-            matriz_valores.append(fila_valores)
+        # esta codicion es para validad que los campos de texto no esten vacios, si lo estan no se ejecuta el metodo
+        if(txt_size != "" and validacion_campos_vacios(matrix_container,txt_size)):
 
-        #imprimir(matriz_valores)
+            matriz_valores.clear()    # esto es un preventino por si hay valores almacenados los limpiamos
+        
+            # ciclo para recorrer y obtener los valores de los texfields de la matriz y termino independiente
+            for fila in matrix_container.controls:
+                fila_valores = []
+                for texfield in fila.controls:
+                    fila_valores.append(texfield.value)
+                matriz_valores.append(fila_valores)
+
+            print(matriz_valores)
+
+            # llamamos el mentodo para resolver el sistema de ecuaciones
+            vector_solucion = gj.ejecucion_programa(matriz_valores)
+            resultado = ""
+
+            # recorremos el vector con los resultados del sistema para mostralor en una cadena de texto
+            for i in range(len(vector_solucion)):
+                print(vector_solucion[i])
+
+                if(i == len(vector_solucion) -1):
+                    resultado += "r" + str(i+1) + " = " + str(round(vector_solucion[i], 2))
+                else:
+                    resultado += "r" + str(i+1) + " = " + str(round(vector_solucion[i], 2)) + ", "
+
+            # validacion para cambiar el tamano de la letra dependiendo del numero de incognitas
+            if(len(vector_solucion) <= 3):
+                resultado_matriz.value = resultado
+            elif(len(vector_solucion) <= 5):
+                resultado_matriz.text_size = 10
+                resultado_matriz.width = 300
+                resultado_matriz.value = resultado
+            elif(len(vector_solucion) <= 7):
+                resultado_matriz.text_size = 10
+                resultado_matriz.width = 380
+                resultado_matriz.value = resultado
+            else:
+                resultado_matriz.text_size = 10
+                resultado_matriz.width = 470
+                resultado_matriz.value = resultado
+
+            page.update()
+        
+        else:
+            pass
+
+
+    # metodo para generar valores aleatorios a la matriz y termino indenpendiente
+    def valores_aleatorios(e):
+        
+        # ciclo para recorrer y obtener los valores de los texfields de la matriz y termino independiente
+        for fila in matrix_container.controls:
+            for texfield in fila.controls:
+                if(texfield.value == ""):
+                    texfield.value = random.randint(-10,100)
+
+
+        page.update()
 
     """==========================================================================================================================================================="""
 
@@ -195,9 +291,10 @@ def main(page: ft.Page):
                 spacing=60,
             )
             
-            ],expand=True,
-            alignment = ft.MainAxisAlignment.SPACE_EVENLY,
-            #spacing= 20,
+            ],
+            expand=True,
+            alignment = ft.MainAxisAlignment.CENTER,
+            spacing= 50,
             )
         
         page.views.clear()
@@ -220,9 +317,10 @@ def main(page: ft.Page):
             ft.Row([
                 txt_size, 
                 ft.ElevatedButton("Generar Matriz", height = 40, on_click=generate_matrix, bgcolor= "#4D82BC", color="#FFFFFF", elevation= 5),
-                ft.ElevatedButton(text = "Borrar", width = 100, height = 40, bgcolor= "#4D82BC", color="#FFFFFF", elevation= 5, on_click=obtener_valores_matriz),
+                ft.ElevatedButton(text = "Random", width = 135, height = 40, bgcolor= "#4D82BC", color="#FFFFFF", elevation= 5, on_click= valores_aleatorios),
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
+                spacing= 24.4,
             ),
             ft.Row([
                 matrix_container,
@@ -236,14 +334,22 @@ def main(page: ft.Page):
                 alignment=ft.MainAxisAlignment.CENTER,
                 spacing = 30,
             ),
+            ft.Row([
+                ft.ElevatedButton(text = "Borrar", width = 300, height = 40, bgcolor= "#4D82BC", color="#FFFFFF", elevation= 5, on_click= borrar_segunda_ventana),
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            ),
 
         ],
         expand=True,
-        alignment = ft.MainAxisAlignment.SPACE_EVENLY,
+        alignment = ft.MainAxisAlignment.CENTER,
+        spacing= 30
         )
 
         page.views.clear()
         page.views.append(ft.View(controls= [layout], bgcolor = "#84B6F4"))
+    
+
     
 
     """======================================================== Controlador del manejo de ventanas ==============================================================="""
@@ -251,8 +357,10 @@ def main(page: ft.Page):
     # Manejar los cambios de ruta
     def on_route_change(e: ft.RouteChangeEvent):
         if e.route == "/segunda":
+            boton_borrar(e)
             second_view()
         else:
+            borrar_segunda_ventana(e)
             home_view()
         page.update()
     
--- Evaluacion_1/proyectos/matriz.py
+++ Evaluacion_1/proyectos/matriz.py
@@ -6,51 +6,80 @@ class GaussJordan:
         self.vector = vector
 
     def resolver_sistema_ecuaciones(self):
-        tamano = len(self.vector)
 
         # evitar error de truncamiento
         self.matriz = np.array(self.matriz, dtype=float)
 
         AB = np.concatenate((self.matriz,self.vector), axis = 1)
 
-        for i in range(tamano):
-            maximo_i = np.argmax(abs(AB[i:, i])) + i
-            AB[[i, maximo_i]] = AB[[maximo_i, i]]
-            pivote = AB[i, maximo_i]
-            AB[i] = AB[i] / pivote
+        # Pivoteo parcial por filas
+        tamano = np.shape(AB)
+        n = tamano[0]
+        m = tamano[1]
 
-            for j in range(tamano):
-                if(i != j):
-                    AB[j] = AB[j] - AB[j,i] * AB[i]
+        # Para cada fila en AB
+        for i in range(0,n-1,1):
+            # columna desde diagonal i en adelante
+            columna = abs(AB[i:,i])
+            dondemax = np.argmax(columna)
+            
+            # dondemax no está en diagonal
+            if (dondemax !=0):
+                # intercambia filas
+                temporal = np.copy(AB[i,:])
+                AB[i,:] = AB[dondemax+i,:]
+                AB[dondemax+i,:] = temporal
+                
 
-        x = AB[:, -1]
+        # eliminacion hacia adelante
+        for i in range(0,n-1,1):
+            pivote = AB[i,i]
+            adelante = i + 1
+            for k in range(adelante,n,1):
+                factor = AB[k,i]/pivote
+                AB[k,:] = AB[k,:] - AB[i,:]*factor
+
+
+        # elimina hacia atras
+        ultfila = n-1
+        ultcolumna = m-1
+        for i in range(ultfila,0-1,-1):
+            pivote = AB[i,i]
+            atras = i-1 
+            for k in range(atras,0-1,-1):
+                factor = AB[k,i]/pivote
+                AB[k,:] = AB[k,:] - AB[i,:]*factor
+            # diagonal a unos
+            AB[i,:] = AB[i,:]/AB[i,i]
+        x = np.copy(AB[:,ultcolumna])
+
         return x
 
 
 def transformar_matrizNp(matriz_valores, matriz, vector):
 
-    filas_vector = []
     for f in range(len(matriz_valores)):
         filas_matriz = []
+        filas_vector = []
         for c in range(len(matriz_valores[0])):
 
             # condicion para que cundo c igual "=" no lo tome en cuenta y otras dos para separar la matriz de los terminos independientes
             if(c == len(matriz_valores[0]) - 2):
                 pass
             elif(c == len(matriz_valores[0]) - 1):
-                filas_vector.append(matriz_valores[f][c])
+                filas_vector.append(int(matriz_valores[f][c]))
             else:
-                filas_matriz.append(matriz_valores[f][c])
+                filas_matriz.append(int(matriz_valores[f][c]))
         matriz.append(filas_matriz)
-    vector.append(filas_vector)
+        vector.append(filas_vector)
 
 
-def ejecucion_programa():
+def ejecucion_programa(matriz_valores):
 
     # deifinimos dos listas vacias
     matriz = []
     vector = []
-    matriz_valores = [[1,2,"=",3],[4,5,"=",6]]
+    #matriz_valores = [[1,2,"=",3],[4,5,"=",6]]
 
     # metodo para recorrer la matriz de valores y separar la matriz, de los terminos independientes
     transformar_matrizNp(matriz_valores, matriz, vector)
@@ -59,9 +88,9 @@ def ejecucion_programa():
     a = np.array(matriz)
     b = np.array(vector)
 
-    # creamos un objeto
+    # creamos un objeto y resolvemos el sistema de ecuaciones
     gaussJordan1 = GaussJordan(a,b)
     resultado = gaussJordan1.resolver_sistema_ecuaciones()
-    print(resultado)
+    return resultado
 
-ejecucion_programa()
+#ejecucion_programa()
